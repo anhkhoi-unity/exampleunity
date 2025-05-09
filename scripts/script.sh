40f3c05af6d316bbc737278e66f936b943dab271
Anhkhoi Vu-Nguyen <anhkhoi.vunguyen@unity3d.com>
2025-05-08 21:34:47 -0400

    Update script

--- scripts/script.sh
+++ scripts/script.sh
@@ -3,5 +3,7 @@
 echo "Hello World!"
 #echo "smudge filter lfs failed"
 echo "Bad runner encountered"
-echo "Unity has not been activated"
+#echo "Unity has not been activated"
+echo ". here?"
+echo "even with numbers and symbols"
 nodee hello.js 
