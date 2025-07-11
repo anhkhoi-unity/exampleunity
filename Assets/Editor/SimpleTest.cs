aea8fde3edf3cbc90113fc6abb7df3a8d39dcc0f
Anhkhoi Vu-Nguyen <anhkhoi.vunguyen@unity3d.com>
2025-07-10 22:13:58 -0400

    update test

--- Assets/Editor/SimpleTest.cs
+++ Assets/Editor/SimpleTest.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
+using System;
 
 /// <summary>
 /// Simple test to isolate the exact issue
@@ -13,12 +14,21 @@ public class SimpleTest : IPreprocessBuildWithReport
     public void OnPreprocessBuild(BuildReport report)
     {
         BuildTargetGroup buildTargetGroup = BuildPipeline.GetBuildTargetGroup(EditorUserBuildSettings.activeBuildTarget);
+        NamedBuildTarget buildTarget = NamedBuildTarget.FromBuildTargetGroup(buildTargetGroup);
         
         Debug.Log("=== SIMPLE TEST START ===");
         
-        // Test the exact pattern you mentioned
-        Debug.Log($"Test 5b: BuildTargetGroup enum interpolation [{buildTargetGroup}] - this should work");
-        Debug.Log($"Test 5b: BuildTargetGroup enum interpolation [{buildTargetGroup}]");
+        // Test 1: Enum implicit cast (should fail according to user)
+        Debug.Log($"Build target group interpolation [{buildTargetGroup}]");
+        
+        // Test 2: String.Format with object property (should fail according to user)  
+        Debug.Log(String.Format("Build target [{0}]", buildTarget.TargetName));
+        
+        // Test 3: Simple string (should work)
+        Debug.Log("Simple log message should work");
+        
+        // Test 4: String concatenation (should work based on previous evidence)
+        Debug.Log("Build target concatenation [" + buildTarget.TargetName + "]");
         
         Debug.Log("=== SIMPLE TEST END ===");
     }
